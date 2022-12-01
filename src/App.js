d90d4895c4c7b5fa0c5acd5f8e05965808a7b7ef
skmudassir-it <96885904+skmudassir-it@users.noreply.github.com>
2022-12-01 13:09:38 +0000

    components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
-import "./App.css";
 import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import LeftBar from "./components/leftbar/LeftBar";
+import RightBar from "./components/rightbar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
-import Register from "./pages/register/Register";
-import { createBrowserRouter, RouterProvider, Route, Navigate } from "react-router-dom";
-import NavBar from "./components/navbar/NavBar";
-import RightBar from "./components/rightbar/RightBar";
-import LeftBar from "./components/leftbar/LeftBar";
-import Outlet from "./components/outlet/Outlet";
+import "./style.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./context/darkModeContext";
+import { AuthContext } from "./context/authContext";
 
 function App() {
-  const currentUser = false;
+  const { currentUser } = useContext(AuthContext);
+
+  const { darkMode } = useContext(DarkModeContext);
+
   const Layout = () => {
     return (
-      <div>
-        <NavBar />
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
+        <Navbar />
         <div style={{ display: "flex" }}>
           <LeftBar />
-          <Outlet />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
           <RightBar />
         </div>
       </div>
     );
   };
-  const ProtectedRoute = ({children}) => {
-    if(!currentUser) {
-      return <Navigate to='/login'/>
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
     }
-    return children
-  }
+
+    return children;
+  };
+
   const router = createBrowserRouter([
     {
       path: "/",
       element: (
         <ProtectedRoute>
-        <Layout />
+          <Layout />
         </ProtectedRoute>
       ),
       children: [
@@ -57,8 +73,9 @@ function App() {
       element: <Register />,
     },
   ]);
+
   return (
-    <div className="App">
+    <div>
       <RouterProvider router={router} />
     </div>
   );
